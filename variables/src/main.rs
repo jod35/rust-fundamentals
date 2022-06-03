bcef3b302370616fc8714e7c3ced8ee3e8c31130
jod35 <jodestrevin@gmail.com>
2022-06-03 08:25:08 -0400

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let x =5;
+
+    let spaces ="knklnlnl";
+
+    println!("{}",spaces);
+
+    println!("The value of x is {}",x);
+
+    let x=6;
+
+    println!("The value of x is {}",x);
+}
